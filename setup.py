1a0a03bb85ca379acbea8bce00a855f24e4c0fb6
halfstackpgr <halfstackpgr@gmail.com>
2024-11-11 22:07:05 +0530

    Setup file

--- setup.py
+++ setup.py
@@ -6,9 +6,9 @@ with open("README.md", "r", encoding="utf-8") as f:
 
 
 setup(
-    name="mailtm-sdk",
-    version="0.1.3",
-    description="Mail.tm Stack Development Kit, designed to enhance your experience with the renowned temporary email service, mail.tm",
+    name="fitgirl",
+    version="0.1.0",
+    description="A small stack of functions that might help to scrap the most popular gaming website, fitgirl-repacks.",
     long_description=description,
     long_description_content_type="text/markdown",
     author="Parth Mishra",
@@ -16,9 +16,8 @@ setup(
     maintainer="Parth Mishra",
     maintainer_email="halfstackpgr@gmail.com",
     url="https://www.github.com/halfstackpgr/fitgirl",
-    packages=find_namespace_packages(include=["mailtm*"]),
-    entry_points={"console_scripts": ["mailtm = mailtm.cli:version"]},
-    install_requires=["aiohttp", "requests", "msgspec", "colorama"],
+    packages=find_namespace_packages(include=["fitgirl*"]),
+    install_requires=["httpx", "bs4"],
     classifiers=[
         "Environment :: Console",
         "Intended Audience :: Developers",
